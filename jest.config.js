be17fa94e5de7bb37331de6e4d57798380cdcd46
Chriss78Vera <chrissvera75@gmail.com>
2024-07-08 23:11:18 -0500

    Update config files

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.(js|jsx)$': 'babel-jest'
   },
   transformIgnorePatterns: [
-    'node_modules/(?!@testing-library)'
+    'node_modules/(?!(your-module)/)'  // Reemplaza 'your-module' con el nombre del módulo que necesita ser transformado, o elimínalo si no es necesario.
   ]
 };
