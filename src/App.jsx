a78362d674857f00771d083f5ad237ec3730c4ae
christopher.vera <christopher.vera@intcomex.com>
2024-07-15 08:47:24 -0500

    ACT: Cambio en la pantall principal

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload pruebas dentro de dev.
+          Hola a todos! <code>src/App.js</code>.
         </p>
         <a
           className="App-link"
